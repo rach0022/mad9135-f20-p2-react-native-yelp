3867144c8574695c352f42a607415e802606eddb
RaviC Rachamalla <rach0022@algonquinlive.com>
2020-12-06 17:58:23 -0500

    updated themes to use commonPatterns for dark/ light

--- styles/themes.js
+++ styles/themes.js
@@ -2,6 +2,25 @@
 
 import * as colours from './colours'
 
+// com mon patterns will be spread throughout dark and light themes
+const commonPatterns = {
+    // used for the stack navigator
+    stackOptions: {
+        headerStyle: { backgroundColor: colours.indigo800 },
+        headerTintColor: colours.gray100
+    },
+    // used for the bottom tab bar navigation
+    tabBarOptions: {
+        activeTintColor: colours.indigo500,
+        inactiveTintColor: colours.indigo100,
+        style: {
+            backgroundColor: colours.gray900
+        }
+    },
+    // used to determine safe area edges
+    safeAreaEdges: ['right', 'left', 'bottom'],
+}
+
 export const light = {
     primaryColor: colours.indigo800,
     accentColor: colours.indigo200,
@@ -11,21 +30,9 @@ export const light = {
     inverseTextColor: colours.white,
     listSeparatorColor: colours.indigo100,
     bodyBackgroundColor: colours.indigo50,
-    // used for the stack navigator
-    stackOptions: {
-        headerStyle: { backgroundColor: colours.primaryColor },
-        headerTintColor: colours.backgroundColor
-    },
-    // used for the bottom tab bar navigation
-    tabBarOptions: {
-        activeTintColor: colours.activeColor,
-        inactiveTintColor: colours.inActiveColor,
-        style: {
-            backgroundColor: colours.backgroundColor
-        }
-    },
-    // used to determine safe area edges
-    safeAreaEdges: ['right', 'left', 'bottom']
+    ...commonPatterns
 }
 
-export const dark = {}
+export const dark = {
+    ...commonPatterns
+}
